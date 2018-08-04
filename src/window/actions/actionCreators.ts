780e2aaa6fa0a51ccffccaa79297566b7aa75de1
Albert Li <liwentao90@yahoo.com>
2018-08-04 05:52:59 -0400

    Window namespace:
    - initially support basic features

---
+++ src/window/actions/actionCreators.ts
@@ -0,0 +1,85 @@
+import {Action} from 'redux-actions';
+import createFSA, {ActionCreator} from '../../utils/createFSA';
+import * as types from "../types";
+import * as actionTypes from "./actionTypes";
+
+export const newWindow:ActionCreator<types.NewWindowPayload> = createFSA<types.NewWindowPayload>(
+    actionTypes.NEW_WINDOWS,
+    (options:types.NewWindowPayload) => <any> options
+);
+
+export const close:ActionCreator<types.ClosePayload> = createFSA<types.ClosePayload>(
+    actionTypes.CLOSE,
+    (options:types.ClosePayload) => <any> options
+);
+
+export const focus:ActionCreator<types.FocusPayload> = createFSA<types.FocusPayload>(
+    actionTypes.FOCUS,
+    (options:types.FocusPayload) => <any> options
+);
+
+export const getBounds:ActionCreator<types.GetBoundsPayload> = createFSA<types.GetBoundsPayload>(
+    actionTypes.GET_BOUNDS,
+    (options:types.GetBoundsPayload) => <any> options
+);
+
+export const getState:ActionCreator<types.GetStatePayload> = createFSA<types.GetStatePayload>(
+    actionTypes.GET_STATE,
+    (options:types.GetStatePayload) => <any> options
+);
+
+export const getOptions:ActionCreator<types.GetOptionsPayload> = createFSA<types.GetOptionsPayload>(
+    actionTypes.GET_OPTIONS,
+    (options:types.GetOptionsPayload) => <any> options
+);
+
+export const hide:ActionCreator<types.HidePayload> = createFSA<types.HidePayload>(
+    actionTypes.HIDE,
+    (options:types.HidePayload) => <any> options
+);
+
+export const minimize:ActionCreator<types.MinimizePayload> = createFSA<types.MinimizePayload>(
+    actionTypes.MINIMIZE,
+    (options:types.MinimizePayload) => <any> options
+);
+
+export const maximize:ActionCreator<types.MaximizePayload> = createFSA<types.MaximizePayload>(
+    actionTypes.MAXIMIZE,
+    (options:types.MaximizePayload) => <any> options
+);
+
+export const moveBy:ActionCreator<types.MoveByPayload> = createFSA<types.MoveByPayload>(
+    actionTypes.MOVE_BY,
+    (options:types.MoveByPayload) => <any> options
+);
+
+export const moveTo:ActionCreator<types.MoveToPayload> = createFSA<types.MoveToPayload>(
+    actionTypes.MOVE_TO,
+    (options:types.MoveToPayload) => <any> options
+);
+
+export const restore:ActionCreator<types.RestorePayload> = createFSA<types.RestorePayload>(
+    actionTypes.RESTORE,
+    (options:types.RestorePayload) => <any> options
+);
+
+export const show:ActionCreator<types.ShowPayload> = createFSA<types.ShowPayload>(
+    actionTypes.SHOW,
+    (options:types.ShowPayload) => <any> options
+);
+
+export const setAsForeground:ActionCreator<types.SetAsForegroundPayload> = createFSA<types.SetAsForegroundPayload>(
+    actionTypes.SET_AS_FOREGROUND,
+    (options:types.SetAsForegroundPayload) => <any> options
+);
+
+export const setBounds:ActionCreator<types.SetBoundsPayload> = createFSA<types.SetBoundsPayload>(
+    actionTypes.SET_BOUNDS,
+    (options:types.SetBoundsPayload) => <any> options
+);
+
+export const updateOptions:ActionCreator<types.UpdateOptionsPayload> = createFSA<types.UpdateOptionsPayload>(
+    actionTypes.UPDATE_OPTIONS,
+    (options:types.UpdateOptionsPayload) => <any> options
+);
+
---
+++ src/window/actions/handlerActionCreator.ts
@@ -0,0 +1,85 @@
+import {Action} from 'redux-actions';
+import createFSA, {ActionCreator} from '../../utils/createFSA';
+import * as types from "../types";
+import * as actionTypes from "./actionTypes";
+
+export const newWindowRes:ActionCreator<types.NewWindowResPayload> = createFSA<types.NewWindowResPayload>(
+    actionTypes.NEW_WINDOWS_RES,
+    (options:types.NewWindowResPayload) => <any> options
+);
+
+export const closeRes:ActionCreator<types.CloseResPayload> = createFSA<types.CloseResPayload>(
+    actionTypes.CLOSE_RES,
+    (options:types.CloseResPayload) => <any> options
+);
+
+export const focusRes:ActionCreator<types.FocusResPayload> = createFSA<types.FocusResPayload>(
+    actionTypes.FOCUS_RES,
+    (options:types.FocusResPayload) => <any> options
+);
+
+export const getBoundsRes:ActionCreator<types.GetBoundsResPayload> = createFSA<types.GetBoundsResPayload>(
+    actionTypes.GET_BOUNDS_RES,
+    (options:types.GetBoundsResPayload) => <any> options
+);
+
+export const getStateRes:ActionCreator<types.GetStateResPayload> = createFSA<types.GetStateResPayload>(
+    actionTypes.GET_STATE_RES,
+    (options:types.GetStateResPayload) => <any> options
+);
+
+export const getOptionsRes:ActionCreator<types.GetOptionsResPayload> = createFSA<types.GetOptionsResPayload>(
+    actionTypes.GET_OPTIONS_RES,
+    (options:types.GetOptionsResPayload) => <any> options
+);
+
+export const hideRes:ActionCreator<types.HideResPayload> = createFSA<types.HideResPayload>(
+    actionTypes.HIDE_RES,
+    (options:types.HideResPayload) => <any> options
+);
+
+export const minimizeRes:ActionCreator<types.MinimizeResPayload> = createFSA<types.MinimizeResPayload>(
+    actionTypes.MINIMIZE_RES,
+    (options:types.MinimizeResPayload) => <any> options
+);
+
+export const maximizeRes:ActionCreator<types.MaximizeResPayload> = createFSA<types.MaximizeResPayload>(
+    actionTypes.MAXIMIZE_RES,
+    (options:types.MaximizeResPayload) => <any> options
+);
+
+export const moveByRes:ActionCreator<types.MoveByResPayload> = createFSA<types.MoveByResPayload>(
+    actionTypes.MOVE_BY_RES,
+    (options:types.MoveByResPayload) => <any> options
+);
+
+export const moveToRes:ActionCreator<types.MoveToResPayload> = createFSA<types.MoveToResPayload>(
+    actionTypes.MOVE_TO_RES,
+    (options:types.MoveToResPayload) => <any> options
+);
+
+export const restoreRes:ActionCreator<types.RestoreResPayload> = createFSA<types.RestoreResPayload>(
+    actionTypes.RESTORE_RES,
+    (options:types.RestoreResPayload) => <any> options
+);
+
+export const showRes:ActionCreator<types.ShowResPayload> = createFSA<types.ShowResPayload>(
+    actionTypes.SHOW_RES,
+    (options:types.ShowResPayload) => <any> options
+);
+
+export const setAsForegroundRes:ActionCreator<types.SetAsForegroundResPayload> = createFSA<types.SetAsForegroundResPayload>(
+    actionTypes.SET_AS_FOREGROUND_RES,
+    (options:types.SetAsForegroundResPayload) => <any> options
+);
+
+export const setBoundsRes:ActionCreator<types.SetBoundsResPayload> = createFSA<types.SetBoundsResPayload>(
+    actionTypes.SET_BOUNDS_RES,
+    (options:types.SetBoundsResPayload) => <any> options
+);
+
+export const updateOptionsRes:ActionCreator<types.UpdateOptionsResPayload> = createFSA<types.UpdateOptionsResPayload>(
+    actionTypes.UPDATE_OPTIONS_RES,
+    (options:types.UpdateOptionsResPayload) => <any> options
+);
+

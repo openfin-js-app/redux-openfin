c2687116b0200fe8b31243e67ae0af194bacd2d6
Albert Li <liwentao90@yahoo.com>
2018-11-02 17:31:33 -0400

    - export async at system pkg

--- src/system/index.ts
+++ src/system/index.ts
@@ -1,5 +1,7 @@
 import * as _actions from './actions/index';
+import * as _async from './async';
 import * as _types from './types';
 
 export const actions:typeof _actions = _actions;
+export const async:typeof _async = _async;
 export const types:typeof  _types = _types;

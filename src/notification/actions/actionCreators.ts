f87341f56a26b305fbde9cc08ccb2ad163f1c3ef
Albert Li <liwentao90@yahoo.com>
2019-01-18 14:26:22 -0500

    - initially support new apis of notification pkg

--- src/notification/actions/actionCreators.ts
+++ src/notification/actions/actionCreators.ts
@@ -3,9 +3,39 @@ import createFSA, {ActionCreator} from '../../utils/createFSA';
 import * as types from "../types";
 import * as actionTypes from "./actionTypes";
 
-export const createNotification:ActionCreator<Partial<types.CreateNotificationPayload>>
+export const createNotification:ActionCreator<types.CreateNotificationPayload>
     =
-createFSA<Partial<types.CreateNotificationPayload>>(
-    actionTypes.CREATE_NOTIFICATION,
-    (options:Partial<types.CreateNotificationPayload>) => <any>(options)
-)
+    createFSA<types.CreateNotificationPayload>(
+        actionTypes.CREATE_NOTIFICATION,
+        (options:Partial<types.CreateNotificationPayload>) => <any>(options)
+    )
+
+
+export const getCurrent:ActionCreator<types.NotificationGetCurrentPayload>
+    =
+    createFSA<types.NotificationGetCurrentPayload>(
+        actionTypes.NOTIFICATION_GET_CURRENT,
+        (options:Partial<types.NotificationGetCurrentPayload>) => <any>(options)
+    )
+
+export const close:ActionCreator<types.NotificationClosePayload>
+    =
+    createFSA<types.NotificationClosePayload>(
+        actionTypes.NOTIFICATION_CLOSE,
+        (options:Partial<types.NotificationClosePayload>) => <any>(options)
+    )
+
+export const sendMessage:ActionCreator<types.NotificationSendMsgPayload>
+    =
+    createFSA<types.NotificationSendMsgPayload>(
+        actionTypes.NOTIFICATION_SEND_MSG,
+        (options:Partial<types.NotificationSendMsgPayload>) => <any>(options)
+    )
+
+export const sendMessageToApplication:ActionCreator<types.NotificationSendMsgToAppPayload>
+    =
+    createFSA<types.NotificationSendMsgToAppPayload>(
+        actionTypes.NOTIFICATION_SEND_MSG_TO_APP,
+        (options:Partial<types.NotificationSendMsgToAppPayload>) => <any>(options)
+    )
+
--- src/notification/types.ts
+++ src/notification/types.ts
@@ -1,21 +1,84 @@
 import {BaseRequestPayload, BaseResponsePayload} from '../base/BasePayload';
 
 export const CREATE_NOTIFICATION_ERROR_MSG = 'OpenFin API call Notification.constructor failed.';
+export const NOTIFICATION_GET_CURRENT_ERROR_MSG = 'OpenFin API call Notification.getCurrent failed.';
+export const NOTIFICATION_CLOSE_ERROR_MSG = 'OpenFin API call Notification.close failed.';
+export const NOTIFICATION_SEND_MSG_ERROR_MSG = 'OpenFin API call Notification.sendMessage failed.';
+export const NOTIFICATION_SEND_MSG_TO_APP_ERROR_MSG = 'OpenFin API call Notification.sendMessageToApplication failed.';
+
+export interface BaseNotificatoinEventResPayload {
+    userId:any
+}
+
+export interface FinNotification {
+    close:(callback?:Function)=>void,
+    sendMessage:(message:string, callback?:Function)=>void,
+    sendMessageToApplication:(message:string, callback?:Function)=>void,
+}
 
 export interface CreateNotificationPayload extends BaseRequestPayload{
+    userId?:any,
     ignoreMouseOver:boolean;
     url:string;
-    message:string;
-    onClick:Function;
-    onClose:Function;
-    onDismiss:Function;
-    onError:Function;
-    onMessage:Function;
-    onShow:Function;
+    message:any;
     timeout:string|number;
     opacity:number;
 }
 
-export interface CreateNotificationResPayload extends BaseResponsePayload{
-    notification:any
+export interface CreateNotificationResPayload extends BaseResponsePayload, BaseNotificatoinEventResPayload{
+    notification:FinNotification
 }
+
+
+export interface NotificationGetCurrentPayload extends BaseRequestPayload {
+}
+export interface NotificationGetCurrentResPayload extends BaseRequestPayload {
+    notification:FinNotification
+}
+
+
+export interface NotificationClosePayload extends BaseRequestPayload {
+}
+export interface NotificationCloseResPayload extends BaseRequestPayload {
+
+}
+
+
+export interface NotificationSendMsgPayload extends BaseRequestPayload {
+    message:any,
+}
+export interface NotificationSendMsgResPayload extends BaseRequestPayload {
+
+}
+
+export interface NotificationSendMsgToAppPayload extends BaseRequestPayload {
+    message:any,
+}
+export interface NotificationSendMsgToAppResPayload extends BaseRequestPayload {
+
+}
+
+
+// notification events
+
+export interface NotificationOnClickResPayload extends BaseResponsePayload, BaseNotificatoinEventResPayload{
+
+}
+export interface NotificationOnCloseResPayload extends BaseResponsePayload, BaseNotificatoinEventResPayload{
+
+}
+export interface NotificationOnDismissResPayload extends BaseResponsePayload, BaseNotificatoinEventResPayload{
+
+}
+export interface NotificationOnErrorResPayload extends BaseResponsePayload, BaseNotificatoinEventResPayload{
+    reason:string,
+    errorObj:any,
+
+}
+export interface NotificationOnMsgResPayload extends BaseResponsePayload, BaseNotificatoinEventResPayload{
+    message:any,
+}
+export interface NotificationOnShowResPayload extends BaseResponsePayload, BaseNotificatoinEventResPayload{
+    successObj:any,
+}
+

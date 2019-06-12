36b7563ecce6b32d7aa7cb6042edbdea043e8d53
Wentao LI <liwentao90@yahoo.com>
2019-06-12 04:46:46 -0400

    - initially refactor event package to adopt api v2 1#

--- src/event/registerDefaultListener.ts
+++ src/event/registerDefaultListener.ts
@@ -1,11 +1,12 @@
 import {Store, Action, Middleware} from 'redux';
+import { FinApplication, FinWindow } from '../GlobalTypes';
 import {appEvents, windowEvents} from './constants';
 import {getStateRes} from '../window/actions/handlerActionCreator';
 
 export default function registerDefaultListener(fin: any, store: Store<any>) {
 
-    const finApplication = fin.desktop.Application.getCurrent();
-    const finWindow = fin.desktop.Window.getCurrent();
+    const finApplication:FinApplication = fin.Application.getCurrentSync();
+    const finWindow:FinWindow = fin.Window.getCurrentSync();
     const {dispatch} = store;
 
     function _dispatch_getStateRes(event:any) {
@@ -20,25 +21,25 @@ export default function registerDefaultListener(fin: any, store: Store<any>) {
         }
     }
 
-    appEvents.forEach((oneEvent)=>{
-        finApplication.addEventListener(oneEvent.name, (event:any)=>{
+    appEvents.forEach(async (oneEvent)=>{
+        await finApplication.addListener(oneEvent.name, (event:any)=>{
             dispatch(oneEvent.actionCreator(event) as any)
         });
     });
 
-    windowEvents.forEach((oneEvent)=>{
+    windowEvents.forEach(async (oneEvent)=>{
         const theName = oneEvent.name;
         if (
             theName == 'maximized'||
             theName == 'minimized'||
             theName == 'restored'
         ){
-            finWindow.addEventListener(theName, (event:any)=>{
+            await finWindow.addListener(theName, (event:any)=>{
                 _dispatch_getStateRes(event);
                 dispatch(oneEvent.actionCreator(event) as any);
             })
         }else{
-            finWindow.addEventListener(theName, (event:any)=>{
+            await finWindow.addListener(theName, (event:any)=>{
                 dispatch(oneEvent.actionCreator(event) as any);
             })
         }

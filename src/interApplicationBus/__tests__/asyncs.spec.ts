e44b7cd159b9927473f37d7ce5927c9503c6a854
Wentao LI <liwentao90@yahoo.com>
2019-06-16 00:03:40 -0400

    - restore system/notification pkg test cases

--- src/interApplicationBus/__tests__/asyncs.spec.ts
+++ src/interApplicationBus/__tests__/asyncs.spec.ts
@@ -1,7 +1,14 @@
 jest.mock('../../utils/createAsyncFun');
-const createAsyncFun = require('../../utils/createAsyncFun');
+const wrapAsyncFun = require('../../utils/wrapAsyncFun');
+
 import * as actions from '../actions';
 import * as asyncs  from '../asyncs';
+
+const getDefaultPromise = ()=>{
+    return new Promise(((resolve, reject) => {
+        resolve({});
+    }))
+}
 
 describe('InterApplication asyncs',()=>{
 
@@ -9,68 +16,54 @@ describe('InterApplication asyncs',()=>{
         jest.resetAllMocks();
     });
 
-    // it('publish',async ()=>{
-    //
-    //     const fin = {
-    //         desktop:{
-    //             InterApplicationBus : {
-    //                 publish:jest.fn((topic,message,succCb,errCb)=>{
-    //                     succCb();
-    //                 })
-    //             }
-    //         }
-    //     };
-    //     const succCb = jest.fn();
-    //     const errCb = jest.fn();
-    //
-    //     createAsyncFun.default=jest.fn(
-    //         (action,ERROR_MSG,resActionCreator,finCb)=>{
-    //             finCb(fin,action,resActionCreator,succCb,errCb);
-    //         }
-    //     );
-    //
-    //     await asyncs.publish(actions.publish({
-    //         topic:'topic',
-    //         message:'message',
-    //     }));
-    //
-    //     expect(fin.InterApplicationBus.publish).toHaveBeenCalled();
-    //     expect(succCb).toHaveBeenCalled();
-    //     expect(createAsyncFun).toMatchSnapshot();
-    //
-    // })
+    it('publish',async ()=>{
 
-    // it('subscribe',async ()=>{
-    //
-    //     const fin = {
-    //         desktop:{
-    //             InterApplicationBus : {
-    //                 subscribe:jest.fn((senderUuid,name,topic,listener,succCb,errCb)=>{
-    //                     succCb();
-    //                 })
-    //             }
-    //         }
-    //     };
-    //     const succCb = jest.fn();
-    //     const errCb = jest.fn();
-    //
-    //     createAsyncFun.default=jest.fn(
-    //         (action,ERROR_MSG,resActionCreator,finCb)=>{
-    //             finCb(fin,action,resActionCreator,succCb,errCb);
-    //         }
-    //     );
-    //
-    //     await asyncs.subscribe(actions.subscribe({
-    //         senderUuid:'senderUuid',
-    //         topic:'topic',
-    //         listener:(message,uuid,name)=>{},
-    //     }));
-    //
-    //     expect(fin.InterApplicationBus.subscribe).toHaveBeenCalled();
-    //     expect(succCb).toHaveBeenCalled();
-    //     expect(createAsyncFun).toMatchSnapshot();
-    //
-    // })
+        const fin = {
+            InterApplicationBus : {
+                publish:jest.fn(getDefaultPromise),
+            }
+        };
+
+        wrapAsyncFun.default=jest.fn(
+            async (action,resActionCreator,finCb)=>{
+                return await finCb(fin,action,resActionCreator);
+            }
+        );
+
+        await asyncs.publish(actions.publish({
+            topic:'topic',
+            message:'message',
+        }));
+
+        expect(fin.InterApplicationBus.publish).toHaveBeenCalled();
+        expect(wrapAsyncFun.default).toHaveBeenCalled();
+
+    })
+
+    it('subscribe',async ()=>{
+
+        const fin = {
+            InterApplicationBus : {
+                subscribe:jest.fn(getDefaultPromise),
+            }
+        };
+
+        wrapAsyncFun.default=jest.fn(
+            async (action,resActionCreator,finCb)=>{
+                return await finCb(fin,action,resActionCreator);
+            }
+        );
+
+        await asyncs.subscribe(actions.subscribe({
+            senderUuid:'senderUuid',
+            topic:'topic',
+            listener:(message,uuid,name)=>{},
+        } as any));
+
+        expect(fin.InterApplicationBus.subscribe).toHaveBeenCalled();
+        expect(wrapAsyncFun.default).toHaveBeenCalled();
+
+    })
 
 
 });
